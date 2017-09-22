b3a104a6004f1f39fe3d9aa86581c5c16224246a
Jose Franny Rodriguez <icancode911@gmail.com>
2017-09-22 15:15:31 -0400

    completed front end of the portfolio

--- public/javascripts/helpers.js
+++ public/javascripts/helpers.js
@@ -1,4 +1,10 @@
-$('.feature').click(function(event){
-    let $newImg = event.target.attributes[1].value;
-    $('#projectImg').attr('src',`images/Bettergram/${$newImg}.gif`)
+
+$('.feature').click(function (event) {
+  let $newImg = event.target.attributes[1].value
+  $('#projectImg').attr('src', `images/Bettergram/${$newImg}.gif`)
 })
+$('.feature2').click(function (event) {
+  let $newImg = event.target.attributes[1].value
+  $('#projectImg2').attr('src', `images/AlphaBlog/${$newImg}.gif`)
+})
+
