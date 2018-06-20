087dddc8450890b9d4293308114e64032a99eb17
Jose Franny Rodriguez <icancode911@gmail.com>
2018-06-19 23:11:14 -0400

    Set up sequelize, postgres and the route for posts

--- routes/index.js
+++ routes/index.js
@@ -1,20 +1,49 @@
 var express = require('express');
-//Require PG and Sequelize For Migrations
 var router = express.Router();
+//Require PG and Sequelize For Migrations
+const models = require('../models');
+const Post = models.post;
+
+// const testpost = Post.build({
+//   body:'Its fun when you put together something that you thought you could no do',
+//   alias:'Yayy!'
+// }).save().then((newpost)=>{
+//   console.log(newpost);
+// })
+
+
+ router.get( '/', function ( req, res ) {
+   Post.findAll().then( function ( posts ) {
+     res.render( 'index', {
+       posts: posts
+     } );
+   } );
+ } );
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index');
-});
-
-router.get('/about', function(req,res,next){
+// router.get( '/', ( req, res, next ) => {
+//   const allPost = Post.findAll()
+//     .then((res,posts)=>{
+//       // res.render( 'index',posts );
+//       res.send(posts);
+//   })
+//   res.render('index');
+// });
+router.post('/add',(req,res)=>{
+  const newpost = Post.build({
+    body:req.body.body,
+    alias:req.body.alias
+  }).save().then((lognewPost)=>{console.log( lognewPost)})
+  res.redirect('/');
+})
+router.get('/about', (req,res,next)=>{
   res.render('about');
 });
 
-router.get('/projects',function(req,res,next){
+router.get('/projects',(req,res,next)=>{
   res.render('projects');
 });
-router.get('/contact',function(req,res,next){
+router.get('/contact',(req,res,next)=>{
   res.render('contact');
 })
 module.exports = router;
