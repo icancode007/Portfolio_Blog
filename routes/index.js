fcadad6e12bcc01ac9a85a762f5e0407c353665a
Jose Franny Rodriguez <icancode911@gmail.com>
2019-09-21 00:10:18 -0400

    clean up and more fixes

--- routes/index.js
+++ routes/index.js
@@ -1,34 +1,34 @@
-var express = require('express');
-var router = express.Router();
+const express = require('express');
+const router = express.Router();
 
-const models = require('../models');
-const Post = models.post;
+// const models = require('../models');
+// const Post = models.post;
 
+router.get( '/', (req, res) => res.render('index'));
 
- router.get( '/', function ( req, res ) {
-   Post.findAll().then( function (posts) {
-     res.render( 'index', {posts} );
-    });
- });
+//  router.get( '/', function ( req, res ) {
+//    Post.findAll().then( function (posts) {
+//      res.render( 'index', {posts} );
+//     });
+//  });
  
-//Post Form Handler Route
-router.post('/post',(req,res)=>{
-  const newpost = Post.build({
-    body:req.body.body,
-    alias:req.body.alias
-  }).save().then((lognewPost)=>{
-    res.redirect( '/' );
-  })
+// //Post Form Handler Route
+// router.post('/post',(req,res)=>{
+//   const newpost = Post.build({
+//     body:req.body.body,
+//     alias:req.body.alias
+//   }).save().then((lognewPost)=>{
+//     res.redirect( '/' );
+//   })
   
-})
-router.get('/about', (req,res,next)=>{
-  res.render('about');
-});
+// })
 
-router.get('/projects',(req,res,next)=>{
+router.get('/projects',(req, res, next)=>{
   res.render('projects');
 });
-router.get('/contact',(req,res,next)=>{
+
+router.get('/contact',(req, res, next)=>{
   res.render('contact');
 })
+
 module.exports = router;
