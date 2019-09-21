388ec8e18a94da09e7f473a2ddbf46103d25cc6e
Jose Franny Rodriguez <icancode911@gmail.com>
2019-09-21 00:03:17 -0400

    update page and prepare for future contact form usage

--- public/javascripts/helpers.js
+++ public/javascripts/helpers.js
@@ -1,19 +1,27 @@
-let $showing  = false;
+// let $showing  = false;
 
-$( '#postForm' ).hide();
-$('.feature').click((event)=> {
+$('#postForm').hide();
+$('#contactMe').hide();
+
+$('.feature').click(event => {
   let $newImg = event.target.attributes[1].value
   $('#projectImg').attr('src', `images/Bettergram/${$newImg}.gif`)
 })
-$('.feature2').click((event)=> {
+$('.feature2').click(event => {
   let $newImg = event.target.attributes[1].value
   $('#projectImg2').attr('src', `images/AlphaBlog/${$newImg}.gif`)
 })
 
-$('#commentToggle' ).click((evt)=>{
-  if($showing){
-    $('#postForm').hide();
-  }else{
-    $('#postForm').show();
-  }
+// $('#commentToggle').click(event => {
+//   if($showing){
+//     $('#postForm').hide();
+//   }else{
+//     $('#postForm').show();
+//   }
+// })
+
+$('#mailTo').click(() => {
+  $('#contactMe').show();
 })
+
+
